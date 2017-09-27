cccbe03d3af1734845805eb1df0f18a46e677c0b
KrunalLathiya <krunallathiya21@gmail.com>
2017-09-27 17:53:46 +0530

    add bootstrap classes

--- vuejs-project/main.js
+++ vuejs-project/main.js
@@ -20,13 +20,17 @@ const router = new VueRouter({
 new Vue({
   router,
   template: `
-    <div id="app">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-      </ul>
-      <router-view></router-view>
+    <div class="container">
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item"><router-link to="/" class="nav-link">Home</router-link></li>
+            <li class="nav-item"><router-link to="/about" class="nav-link">About</router-link></li>
+            <li class="nav-item"><router-link to="/contact" class="nav-link">Contact</router-link></li>
+          </ul>
+        </div>
+      </nav>
+      <router-view class="view"></router-view>
     </div>
   `
 }).$mount('#app')
